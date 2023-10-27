78e169f6d487f3b5940eb7b4409428130781c310
George <gdiwan123@gmail.com>
2023-10-26 20:13:20 -0400

    Add client to send emails using gmail

---
+++ src/helpers/emailClient.py
@@ -0,0 +1,19 @@
+import smtplib
+from email.mime.text import MIMEText
+import config.configuration as c
+from config.logging_config import logging
+
+appConfig = c.Config(c.STAGE)
+
+def send_email(to, body):
+    msg = MIMEText(body)
+    msg[c.FROM] = appConfig.smtp_user
+    msg[c.TO] = to
+
+    logging.info(f"Sending email to {to}...")
+
+    server = smtplib.SMTP(appConfig.smtp_server, appConfig.smtp_port)
+    server.starttls()
+    server.login(appConfig.smtp_user, appConfig.password)
+    server.sendmail(appConfig.smtp_user, to, msg.as_string())
+    server.quit()
